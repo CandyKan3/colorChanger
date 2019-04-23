94cd03abef1246fb16b3913f5c16a6c6ec4a6259
Bryce <brycekane5@gmail.cojm>
2019-04-23 16:29:52 -0400

    first commit:

---
+++ src/colorthingy/Colorthingy.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colorthingy;
+
+/**
+ *
+ * @author homefolder
+ */
+public class Colorthingy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        jframe myFrame = new jframe();
+        myFrame.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
